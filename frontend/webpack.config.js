630e12a5e81ca10d6bdd837ad94757ca29263fa3
Jakob <jakob.abfalter@brex.io>
2021-09-25 12:22:57 +0200

    [Dev] Templating now works

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,16 +10,18 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env'],
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
                 }
             }
-        }],
+        ],
     },
     plugins: [
         new HtmlWebPackPlugin({
